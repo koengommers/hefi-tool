01525694ccbb4d943a19cb4fd7818c3d4b445eaf
Koen Gommers <koen@koengommers.nl>
2019-06-06 13:06:39 +0200

    Initial commit

---
+++ core/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+DB_URL = 'sqlite:///data/database.sqlite'
+
+class Database:
+
+    def __init__(self, url):
+        self.engine = create_engine(url)
+        self.Model = declarative_base()
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
+
+    def create_all(self):
+        self.Model.metadata.create_all(self.engine)
+
+db = Database(DB_URL)
