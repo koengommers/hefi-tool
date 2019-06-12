f34d9fe02e4606aeb9a2390f982541c5c3e95955
Koen Gommers <koen@koengommers.nl>
2019-06-12 13:25:12 +0200

    Minor improvements to finance document

--- pipeline/processing/finance_query.py
+++ pipeline/processing/finance_query.py
@@ -16,14 +16,11 @@ class FinanceDocument(object):
         """
         Build a query for extracting indexes.
         """
-        q = []
-        for index in self.data:
-            q.append((index,'LTTextLineHorizontal:contains("' + index + '")'))
-        return q
+        return [(index, 'LTTextLineHorizontal:contains("' + index + '")') for index in self.data]
 
-    def chose(self, res):
+    def choose(self, res):
         """
-        Chose indexes with a bounding box of certain heigth.
+        Choose indexes with a bounding box of certain heigth.
         This way no titles are chosen.
         """
         for r in res:
@@ -36,36 +33,35 @@ class FinanceDocument(object):
         Get coordinates of the two columns in a financial document.
         """
         result = self.pdf.extract([
-            ('with_parent','LTPage[page_index="1"]'),
-            ('column 1','LTTextLineHorizontal:contains("Bedrag in euro\'s per einde verslagjaar")'),
-            ('column 2','LTTextLineHorizontal:contains("Bedrag in euro\'s per einde vorig")')
+            ('with_parent', 'LTPage[page_index="1"]'),
+            ('column 1', 'LTTextLineHorizontal:contains("Bedrag in euro\'s per einde verslagjaar")'),
+            ('column 2', 'LTTextLineHorizontal:contains("Bedrag in euro\'s per einde vorig")')
         ])
         coords = []
         for res in result.values():
-            coords.append([float(x) for x in str(res)[29:60].split(", ")[:4]])
+            coords.append([float(x) for x in res[0].get('bbox').strip('[]').split(', ')])
         return coords
 
     def get_good(self, result):
         """
-        When multiple results of certain index chose the first occuring.
+        When multiple results of certain index choose the first occurring.
         """
         new = dict()
         for index, res in result.items():
-           new[index] = self.chose(res)
+           new[index] = self.choose(res)
         return new
 
     def get_bboxes(self, result, cols):
         """
-        Get bouding boxes of values of indexes in retrieved columns.
+        Get bounding boxes of values of indexes in retrieved columns.
         """
         new = dict()
         for index, res in result.items():
             if res is None:
-                print(index, res)
                 new[index] = None
             else:
                 page_index = next(res.iterancestors('LTPage')).get('page_index', 0)
-                x,y,x2,y2 = [float(x) for x in re.split(",| ", str(res.layout)[22:52])[:4]]
+                x, y, x2, y2 = [float(x) for x in res.get('bbox').strip('[]').split(', ')]
                 new[index] = ([cols[0][0], y, cols[0][2], y2], [cols[1][0], y, cols[1][2], y2], page_index)
         return new
 
@@ -78,16 +74,15 @@ class FinanceDocument(object):
         for index, coord in bboxes.items():
             if coord is not None:
                 page_id = coord[2]
-                coord_1 = str(coord[0])[1:-1]
-                coord_2 = str(coord[1])[1:-1]
+                coord_1 = ', '.join(map(str, coord[0]))
+                coord_2 = ', '.join(map(str, coord[1]))
             else:
                 page_id = '0'
-                coord_1 = '0, 0, 0, 0'
-                coord_2 = '0, 0, 0, 0'
-            col_q1.append(('with_parent','LTPage[page_index="' + page_id + '"]'))
-            col_q2.append(('with_parent','LTPage[page_index="' + page_id + '"]'))
-            col_q1.append((index, 'LTTextLineHorizontal:overlaps_bbox("'+coord_1+'")'))
-            col_q2.append((index, 'LTTextLineHorizontal:overlaps_bbox("'+coord_2+'")'))
+                coord_1 = coord_2 = '0, 0, 0, 0'
+            col_q1.append(('with_parent','LTPage[page_index="{}"]'.format(page_id)))
+            col_q2.append(('with_parent','LTPage[page_index="{}"]'.format(page_id)))
+            col_q1.append((index, 'LTTextLineHorizontal:overlaps_bbox("{}")'.format(coord_1)))
+            col_q2.append((index, 'LTTextLineHorizontal:overlaps_bbox("{}")'.format(coord_2)))
         return col_q1, col_q2
 
     def query(self, q):
@@ -97,7 +92,6 @@ class FinanceDocument(object):
         result = self.get_good(self.pdf.extract(q))
         cols = self.get_cols()
         bboxes = self.get_bboxes(result, cols)
-        print(bboxes,'\n')
         col_q1, col_q2 = self.build_col_q(bboxes)
         one = self.pdf.extract(col_q1)
         two = self.pdf.extract(col_q2)
