ef7f337b98826dbf7bebc07c474c9cf9827cd017
Koen Gommers <koen@koengommers.nl>
2019-06-18 13:32:36 +0200

    Improved document downloader

--- pipeline/retrieval/document_downloader.py
+++ pipeline/retrieval/document_downloader.py
@@ -4,6 +4,7 @@ from itertools import groupby
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException, NoSuchWindowException
 
 from pipeline.retrieval.documents_page import DocumentsPage
 from pipeline.retrieval.overview_page import OverviewPage
@@ -21,10 +22,11 @@ class DocumentDownloader:
 
     def split_documents(self):
         for doc in self.documents:
-            if doc.standardized and not doc.downloaded:
-                self.standardized.append(doc)
-            elif not doc.downloaded:
-                self.non_standardized.append(doc)
+            if doc:
+                if doc.standardized and not doc.downloaded:
+                    self.standardized.append(doc)
+                elif not doc.downloaded:
+                    self.non_standardized.append(doc)
 
     def init_driver(self):
         chrome_options = Options()
@@ -33,6 +35,7 @@ class DocumentDownloader:
                 "enabled": False,
                 "name": "Chrome PDF Viewer"
             }],
+            "plugins.always_open_pdf_externally": True,
             "download": {
                 "prompt_for_download": False,
                 "default_directory": self.download_dir
@@ -47,6 +50,19 @@ class DocumentDownloader:
             self.download_non_standardized()
             self.driver.quit()
 
+    def check_directory(self):
+
+        if len(os.listdir(self.download_dir)) > 0:
+            print('{} directory not empty'.format(self.download_dir))
+            clear_directory = input('Want to remove its contents? [y/n] ') == 'y'
+            if clear_directory:
+                for file in os.listdir(self.download_dir):
+                    filepath = os.path.join(self.download_dir, file)
+                    if os.path.isfile(filepath):
+                        os.unlink(filepath)
+            else:
+                exit()
+
     def download_standardized(self):
         entry_groups = [list(group) for entry_id, group in groupby(self.standardized, lambda doc: doc.entry_id)]
         entry_groups = [(group[0].entry, group) for group in entry_groups]
@@ -54,23 +70,29 @@ class DocumentDownloader:
             page = DocumentsPage(entry)
             self.driver.get(page.url)
             for doc in docs:
-                if len(os.listdir(self.download_dir)) > 0:
-                    raise RuntimeError('{} directory not clean'.format(self.download_dir))
-                elem = self.driver.find_element_by_xpath('//a[@class="DocumentReportItem" and text() = "{}"]'.format(doc.label))
-                elem.click()
+                self.check_directory()
+                try:
+                    elem = self.driver.find_element_by_xpath('//a[@class="DocumentReportItem" and text() = "{}"]'.format(doc.label))
+                    elem.click()
 
-                time.sleep(0.1)
-                self.driver.switch_to.window(self.driver.window_handles[-1])
-                if self.driver.title == 'Fout pagina':
-                    self.driver.close()
-                    self.driver.switch_to.window(self.driver.window_handles[0])
-                else:
+                    time.sleep(0.1)
+                    try:
+                        self.driver.switch_to.window(self.driver.window_handles[-1])
+                        if self.driver.title == 'Fout pagina':
+                            print('Document #{} can\'t be downloaded'.format(doc.id))
+                            self.driver.close()
+                            self.driver.switch_to.window(self.driver.window_handles[0])
+                            continue
+                    except:
+                        pass
                     self.driver.switch_to.window(self.driver.window_handles[0])
                     while not [f for f in os.listdir(self.download_dir) if f.endswith('.pdf')]:
                         time.sleep(0.1)
                     filename = next(f for f in os.listdir(self.download_dir) if f.endswith('.pdf'))
                     os.rename(os.path.join(self.download_dir, filename), os.path.join(self.document_dir, doc.filename()))
                     doc.set_downloaded()
+                except NoSuchElementException:
+                    print('Document #{} can\'t be downloaded'.format(doc.id))
 
     def download_non_standardized(self):
         year_groups = [(year, list(group)) for year, group in groupby(self.non_standardized, lambda doc: doc.entry.year)]
