22ad1688c7910e1c7487035beee75351b542eaf1
Koen Gommers <koen@koengommers.nl>
2019-06-29 01:33:30 +0200

    Fix for installing

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ import setuptools
 setuptools.setup(
     name="hefi_tool",
     version="1.0.0",
-    packages=['hefi_tool'],
+    packages=[
+        'hefi_tool',
+        'hefi_tool.models',
+        'hefi_tool.processing',
+        'hefi_tool.retrieval'
+    ],
     install_requires=[
         'sqlalchemy',
         'alembic',
